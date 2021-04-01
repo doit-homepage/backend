c66307991d49317ff71316bc7cd85165e4d3b005
gaeunU <dbrkdmsyge12@naver.com>
2021-04-01 19:49:17 +0900

    commit message

--- routes/study.js
+++ routes/study.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*스터디 목록 조회 */
+router.get('/read/:idx',function(req,res,next)
+{
+var idx = req.params.idx;
+    var sql = "select idx, title, header, content, start_date, end_date, student_num, date, teacher , student_number , name , writer" +
+        "date_format(regdate,'%Y-%m-%d %H:%i:%s') content ,hit from board where idx=?";
+    conn.query(sql,[idx], function(err,row)
+    {
+        if(err) console.error(err);
+        res.render('read', {title:"글 상세", row:row[0]});
+    });
+});
+
 module.exports = router;
