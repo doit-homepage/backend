bb3d5bbb158eee26f0aeb0c28c320b32b30bbf71
i-nooo <ssfoxs@kakao.com>
2021-04-07 13:33:08 +0900

    practice

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+const { decode } = require('jsonwebtoken');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+/* /info/list/:page */ //공지사항 목록 조회
+router.get('list/:page', function(req, res, next) {
+    var info_list = [];
+    var query = 'select title, head, writer, date, like from doit.study where study.id'
+    var data = await db.sequelize.query(query, {type: db.sequelize.QueryTypes.SELECT});
+
+    for (var s of data) {
+        study_list.push({
+            title: s.title,
+            head: s.head,
+            writer: s.writer,
+            date: s.date,
+            like: s.like
+        });
+    }res.json({ success: true, study_list: study_list })
 });
 
 module.exports = router;
--- routes/study.js
+++ routes/study.js
@@ -1,9 +1,31 @@
 var express = require('express');
+const study = require('../models/study');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+/* /study/list/:page */ //스터디 목록 조회
+router.get('list/:page', function(req, res, next) {
+  var study_list = [];
+    var query = 'select title, header, content, start_date, end_date, student_num, date from doit.study where study.id'
+    var study_data = await db.sequelize.query(query, {type: db.sequelize.QueryTypes.SELECT});
+
+    for (var s of study_data) {
+        study_list.push({
+            title: s.title,
+            header: s.header,
+            content: s.content,
+            start_date:s.date,
+            end_date:s.date,
+            student_num: s.num,
+            date: s.date
+        });
+    }res.json({ success: true, study_list: study_list })
 });
 
 module.exports = router;
