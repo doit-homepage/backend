ff80a54a563482b1067732f864fa896749416ef6
i-nooo <ssfoxs@kakao.com>
2021-04-10 19:04:31 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,20 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 /* /info/list/:page */ //공지사항 목록 조회
-router.get('list/:page', function(req, res, next) {
+router.get('list/:page', function(req, res, next) { //🤔
     var info_list = [];
-    var query = 'select title, head, writer, date, like from doit.study where study.id'
+    var query = 'select title, head, writer, date, like from doit.study where notice.id'
     var data = await db.sequelize.query(query, {type: db.sequelize.QueryTypes.SELECT});
 
     for (var s of data) {
-        study_list.push({
+        info_list.push({
             title: s.title,
             head: s.head,
             writer: s.writer,
             date: s.date,
             like: s.like
         });
-    }res.json({ success: true, study_list: study_list })
+    }res.json({ success: true, info_list: info_list })
 });
 
 module.exports = router;
