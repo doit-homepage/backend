7d97360db47c89a58038af7ab42914f47d8ce24b
jang-eun-ju <dmswn0261@ajou.ac.kr>
2021-05-10 00:08:55 +0900

    study.js 추가

--- routes/study.js
+++ routes/study.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/index')
+var crypto = require('crypto')
+var jwt = require('jsonwebtoken')
+var async = require('async')
+
+var sequelize = require('sequelize')
+
+var Study = db.Study
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function (req, res, next) {
+  console.log("post");
+  Study.create({ 
+    title: req.body.title, 
+    header: req.body.header, 
+    content: req.body.content, 
+    date: req.body.date,
+    writer: req.body.writer,
+  })
+    .then((data) => { res.json({ success: true, data }) })
+    .catch((err) => {
+      if (err) return res.json({ success: false, err })
+    })
 });
 
 module.exports = router;
