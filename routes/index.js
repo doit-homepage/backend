3d41f7a65a1621e0e94a021049d0cdbb7f3c6d8c
Kim HyeGyeong <51890074+i-nooo@users.noreply.github.com>
2021-05-10 13:57:25 +0900

    page

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,25 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 /* /info/list/:page */ //공지사항 목록 조회
-router.get('list/:page', function(req, res, next) { //🤔
+router.get('/list/:page', function(req, res, next) { //🤔 sqld
     var info_list = [];
-    var query = 'select title, header, writer, date from doit.notice where notice.id'
+    var query = 'select notice.title, notice.header, notice.writer, notice.date from doit.notice order by notice.date desc'
     var data = await db.sequelize.query(query, {type: db.sequelize.QueryTypes.SELECT});
 
+	var page = params.page
+	var count = 0;
     for (var s of data) {
-        info_list.push({
-            title: s.title,
-            header: s.header,
-            writer: s.writer,
-            date: s.date,
-            like: s.like
-        });
-    }res.json({ success: true, info_list: info_list })
+		if(count >= 10 * (page-1) + 1 && count <= page * 10) {
+			info_list.push({
+				title: s.title,
+				header: s.header,
+				writer: s.writer,
+				date: s.date,
+				like: s.like
+			});
+		} 
+    }
+	res.json({ success: true, info_list: info_list })
 });
 
 module.exports = router;
--- routes/study.js
+++ routes/study.js
@@ -15,7 +15,11 @@ router.get('list/:page', function(req, res, next) {
     var query = 'select title, header, content, start_date, end_date, student_num, date from doit.study where study.id'
     var study_data = await db.sequelize.query(query, {type: db.sequelize.QueryTypes.SELECT});
 
+    var page = params.page
+    var count = 0;
+
     for (var s of study_data) {
+      if(count >= 10 * (page-1) + 1 && count <= page * 10) {
         study_list.push({
             title: s.title,
             header: s.header,
@@ -25,6 +29,7 @@ router.get('list/:page', function(req, res, next) {
             student_num: s.num,
             date: s.date
         });
+      }
     }res.json({ success: true, study_list: study_list })
 });
 
