aaaa5e12d50a8d41c32b60d0345386cb7c9323ee
jang-eun-ju <dmswn0261@ajou.ac.kr>
2021-05-19 10:45:15 +0900

    token

--- routes/info.js
+++ routes/info.js
@@ -6,22 +6,25 @@ var jwt = require('jsonwebtoken')
 var async = require('async')
 
 var sequelize = require('sequelize')
-
+const { decode } = require('punycode');
 var Notice = db.Notice
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-  console.log("post");
-  Notice.create({ 
-    title: req.body.title, 
-    header: req.body.header, 
-    content: req.body.content, 
-    date: req.body.date,
-    writer: req.body.writer,
-  })
-    .then((data) => { res.json({ success: true, data }) })
-    .catch((err) => {
-      if (err) return res.json({ success: false, err })
+  var token = req.headers['x-access-token']
+  jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
+    Study.create({
+      title: req.body.title,
+      header: req.body.header,
+      content: req.body.content,
+      date: req.body.date,
+      writer: decoded.id,
     })
+      .then((data) => { res.json({ success: true, data }) })
+      .catch((err) => {
+        if (err) return res.json({ success: false, err })
+      })
+
+  })
 });
 module.exports = router;
--- routes/study.js
+++ routes/study.js
@@ -5,24 +5,29 @@ var crypto = require('crypto')
 var jwt = require('jsonwebtoken')
 var async = require('async')
 
-var sequelize = require('sequelize')
+var sequelize = require('sequelize');
+const { decode } = require('punycode');
 
 var Study = db.Study
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-  console.log("post");
-  Study.create({ 
-    title: req.body.title, 
-    header: req.body.header, 
-    content: req.body.content, 
-    date: req.body.date,
-    writer: req.body.writer,
-  })
-    .then((data) => { res.json({ success: true, data }) })
-    .catch((err) => {
-      if (err) return res.json({ success: false, err })
+  var token = req.headers['x-access-token']
+  jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
+    console.log("post");
+    Study.create({
+      title: req.body.title,
+      header: req.body.header,
+      content: req.body.content,
+      date: req.body.date,
+      writer: decoded.id,
+      student_num: req.body.student_num
     })
+      .then((data) => { res.json({ success: true, data }) })
+      .catch((err) => {
+        if (err) return res.json({ success: false, err })
+      })
+  })
 });
 
 module.exports = router;
