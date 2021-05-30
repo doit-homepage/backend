895de62f75f07af47a8961eaff08910725d74a5f
gaeunU <dbrkdmsyge12@naver.com>
2021-05-31 02:46:33 +0900

    수정

--- routes/info.js
+++ routes/info.js
@@ -16,7 +16,7 @@ var NoticeLike = db.NoticeLike;
 
 router.get("/:id", async function (req, res, next) {
   var values = {
-    id: params.id,
+    id: Number(req.params.id),
   };
 
   var NoticeDetailData = [];
@@ -58,22 +58,23 @@ router.post("/like", async function (req, res, next) {
   }
 });
 
-router.post('/', function (req, res, next) {
-  var token = req.headers['x-access-token']
+router.post("/", function (req, res, next) {
+  var token = req.headers["x-access-token"];
   jwt.verify(token, process.env.JWT_KEY, function (err, decoded) {
     Notice.create({
       title: req.body.title,
       header: req.body.header,
       content: req.body.content,
       date: req.body.date,
-      writer: decoded.id
-  })
-      .then((data) => { res.json({ success: true, data }) })
-      .catch((err) => {
-        if (err) return res.json({ success: false, err })
+      writer: decoded.id,
+    })
+      .then((data) => {
+        res.json({ success: true, data });
       })
-
-  })
+      .catch((err) => {
+        if (err) return res.json({ success: false, err });
+      });
+  });
 });
 
 module.exports = router;
